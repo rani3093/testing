fc158767caab05aeb36389d4e4239e25ebd053f2
Archana <archana.vetkar@vedicrishiastro.com>
2019-11-18 12:33:06 +0530

    first commit

---
+++ MyNewProjectDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyNewProjectDemo
+//
+//  Created by Vedic Rishi on 18/11/19.
+//  Copyright © 2019 Vedic Rishi Astro. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World! this is test")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
